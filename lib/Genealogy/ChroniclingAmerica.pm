0abab920077a454b90c65ed81c2fbb748f731b6d
Nigel Horne <njh@bandsman.co.uk>
2019-11-17 09:59:27 -0500

    Release 0.03

--- lib/Genealogy/ChroniclingAmerica.pm
+++ lib/Genealogy/ChroniclingAmerica.pm
@@ -14,11 +14,11 @@ Genealogy::ChroniclingAmerica - Find URLs for a given person on the Library of C
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
